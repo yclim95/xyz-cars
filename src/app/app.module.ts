307f76e4f2ece4a325b9ed7531a45a657f81947e
yclim95 <lyc950421@gmail.com>
2020-07-27 08:10:37 +0800

    Create cars & car detail Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    CarsComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule
